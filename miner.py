29af65cfb2e10ef36b8af137e2f6ecc58f15d11f
Shagun <sshagunsodhani@gmail.com>
2014-11-25 10:15:51 +0530

    fixed indentation error and a minor syntax error

--- miner.py
+++ miner.py
@@ -19,43 +19,43 @@ class Miner(object):
 		self.instance_superset = {}
 		self.candidate_sizeone = {}
 		self.conn = db.connect(app_name)
-		self.cursor = conn.cursor()
-		initialise()
-    
-    def initialise(self):
-    	"""To initialise the class variables"""
-    	self.mapping = json.load(self.mappingFile)
-    	self.instance_superset = json.load(self.inFile)
-    	sql_candidate = "INSERT INTO candidate (colocation, pi) values "
-    	sql_location = "INSERT INTO location (instanceid, x, y) values "
-    	count = 1
-    	for i in self.instance_superset:
-    		sql_location +="("+str(i)+","+str(self.instance_superset[i['x_location']])+","+str(self.instance_superset[i['y_location']])+"),"
-    		count = (count+1)
-    		if(count%5000 == 0):
-    			if(sql_location[-1]==','):
-    				sql_location=sql_location[:-1]
-    			db.write(sql_location, self.cursor, self.conn)
-    			sql_location = "INSERT INTO location (instanceid, x, y) values "
-    			print count, "Items inserted into location table"
-	    if(sql_location[-1]==','):
+		self.cursor = self.conn.cursor()
+		self.initialise()
+
+	def initialise(self):
+		"""To initialise the class variables"""
+		self.mapping = json.load(self.mappingFile)
+		self.instance_superset = json.load(self.inFile)
+		sql_candidate = "INSERT INTO candidate (colocation, pi) values "
+		sql_location = "INSERT INTO location (instanceid, x, y) values "
+		count = 1
+		for i in self.instance_superset:
+			sql_location +="("+str(i)+","+str(self.instance_superset[i['x_location']])+","+str(self.instance_superset[i['y_location']])+"),"
+			count = (count+1)
+			if(count%5000 == 0):
+				if(sql_location[-1]==','):
+					sql_location=sql_location[:-1]
+				db.write(sql_location, self.cursor, self.conn)
+				sql_location = "INSERT INTO location (instanceid, x, y) values "
+				print count, "Items inserted into location table"
+		if(sql_location[-1]==','):
 			sql_location=sql_location[:-1]
 		db.write(sql_location, self.cursor, self.conn)		
-	    
-	    count = 1
-	    for i in self.mapping :
-	    	sql_candidate = "("+str(self.mapping[i])+",1),"
-	    	count = (count+1)
-    		if(count%5000 == 0):
-    			if(sql_candidate[-1]==','):
-    				sql_candidate=sql_candidate[:-1]
-    			db.write(sql_candidate, self.cursor, self.conn)
-    			sql_candidate = "INSERT INTO candidate (colocation, pi) values "
-    			print count, "Items inserted into candidate table"		
-    	if(sql_candidate[-1]==','):
+
+		count = 1
+		for i in self.mapping :
+			sql_candidate = "("+str(self.mapping[i])+",1),"
+			count = (count+1)
+			if(count%5000 == 0):
+				if(sql_candidate[-1]==','):
+					sql_candidate=sql_candidate[:-1]
+				db.write(sql_candidate, self.cursor, self.conn)
+				sql_candidate = "INSERT INTO candidate (colocation, pi) values "
+				print count, "Items inserted into candidate table"		
+		if(sql_candidate[-1]==','):
 			sql_candidate=sql_candidate[:-1]
 		db.write(sql_candidate, self.cursor, self.conn)		
 
 
 
-a = Miner(inFile)
+a = Miner()
