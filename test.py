299098a703edda0c73d1b16857b376ddba8d2317
Shagun <sshagunsodhani@gmail.com>
2014-11-26 21:40:52 +0530

    minor changes in test.py

--- test.py
+++ test.py
@@ -2,19 +2,18 @@ from miner import Miner
 import time
 from db import create_db
 
-file_list = []
 schema = open('schema/spatium.sql','r')
 sql = schema.read()
 
 count = 1
-for i in file_list:
+for i in range(1,5):
     sql_db = "CREATE DATABASE spatium"+str(count)
     count+=1
     create_db(sql_db, sql)
     start_time = time.time()
-    mapping = "Input_Preprocessing/"+str(i[1])
-    inputfile = "Input_Preprocessing/"+str(i[0])
-    a = Miner(create_table = 1, minPrevalance = 0.2, threshold_distance = 5000,mappingFile = mapping, inFile = inputfile)
+    mapping = "Input_Preprocessing/mapping"+str(i[1])+".json"
+    inputfile = "Input_Preprocessing/input_preprocessed"+str(i[0])+".json"
+    a = Miner(create_table = 1, minPrevalance = 0.2, threshold_distance = 5000,mappingFile = mapping, inFile = inputfile, quiet = 1)
     a.initialise()
     a.colocation_2()
     a.colocation_k(3)
