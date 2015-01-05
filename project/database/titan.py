14c78aba91700114456bf07458e14e19591989ee
Shagun <sshagunsodhani@gmail.com>
2015-01-05 23:28:11 +0530

    titan handler init

---
+++ project/database/titan.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+#---------------------------------------------------------Import Modules----------------------------------------------------------------------#
+
+import os
+from ConfigParser import ConfigParser
+import math
+
+try :
+    from bulbs.config import Config, DEBUG
+except ImportError as exc:
+    print("Error: failed to import settings module ({})".format(exc))
+
+try:
+    from bulbs.titan import Graph, TITAN_URI
+except ImportError as exc:
+    print("Error: failed to import settings module ({})".format(exc))
+
+
+def connect(app_name = "spatium_titan", config_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../config', 'config.cfg') ):
+
+    '''Open database connection and return Graph object to perform database queries'''
+
+    config=ConfigParser()
+    config.read(config_path)
+    username=config.get(app_name,"user")
+    password=config.get(app_name,"passwd")
+    config = Config(TITAN_URI, username=username, password=password)
+    config.set_logger(DEBUG)
+    g = Graph(config)
+    return g
+    
+connect()
