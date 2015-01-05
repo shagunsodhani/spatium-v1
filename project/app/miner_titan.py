cdb45b918505ef2e929499bd61e703c56949be44
Shagun <sshagunsodhani@gmail.com>
2015-01-05 23:49:53 +0530

    Added method to dump vertices from file to titan

--- project/app/miner_titan.py
+++ project/app/miner_titan.py
@@ -23,16 +23,27 @@ except ImportError as exc:
 
 
 class Miner(object):
-	"""Class to implement Co-location Miner"""	
+	'''Class to implement Co-location Miner'''
 
 
-	def __init__(self, mappingFile = "Data/mapping.json", inFile = "Data/input_preprocessed.json", app_name = "spatium_titan", threshold_distance=1000, minPrevalance = 0.001, kmax = 4, quiet = 0):
+	def __init__(self, mappingFile = "../Data/mapping.json", inFile = "../Data/input_preprocessed.json", app_name = "spatium_titan", threshold_distance=1000, minPrevalance = 0.001, kmax = 4, quiet = 0):
 		
 		self.inFile = inFile
 		self.mappingFile = mappingFile
-		self.mapping = {}
+		# self.mapping = {}
 		# print dbname
 		self.g = db.connect(app_name)
 		self.threshold_distance = threshold_distance
 		self.minPrevalance = minPrevalance
 		self.quiet = quiet
+
+	def insert_vertices_from_file(self):
+		'''To initialise location table'''
+		f_mapping = open(self.mappingFile, 'r')
+		mapping = json.load(f_mapping)
+		count = 1
+		for i in mapping :
+			self.g.vertices.create(id = i,latitude = mapping[i]['latitude'], type = mapping[i]['type'], longitude = mapping[i]['longitude'])
+			count = (count+1)
+		print count, "number of vertices inserted"
+		print self.g.V
