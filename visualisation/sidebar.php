fad1ce3d9e9e5f2781ded6654e17090a41b07d7c
Shagun <sshagunsodhani@gmail.com>
2015-01-20 09:00:14 +0530

    changed the urls in sidebar

--- visualisation/sidebar.php
+++ visualisation/sidebar.php
@@ -1,16 +1,18 @@
 <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-        <li><a href = "">CLEAR MAP</a></li>
+        <li><a href = "index.php?type=clear">CLEAR MAP</a></li>
     
     <?php
-        include 'db.php';
+
+        require_once('db.php');
         $conn = connect();
         $sql = "SELECT DISTINCT primary_type FROM `dataset`";
         $result = query($conn, $sql);
+        $conn = null;
         $count = 1;
         while($row = $result->fetch(PDO::FETCH_ASSOC)) 
         {
-            echo "<li><a href=\"".$count.".html\">".$row['primary_type']."</a></li>\n";
+            echo "<li><a href=\"index.php?type=".$row['primary_type']."\">".$row['primary_type']."</a></li>\n";
             $count++;
         }
     ?>
