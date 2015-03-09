86b30bcd082ee5fb44a424885712ed61ad05688b
sanketvmehta <sanketvmehta@outlook.com>
2015-03-09 11:41:49 +0530

    Mapper.java added to map long crime types to simple numeric types

---
+++ Titan/src/Mapper.java
@@ -0,0 +1,50 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+
+public class Mapper{
+	
+	public HashMap<String, String> enCoding;
+	public HashMap<String, String> deCoding;
+	
+	public Mapper() throws SQLException{
+		enCoding = new HashMap<String,String>();
+		deCoding = new HashMap<String,String>();
+		init();
+	}
+	
+	public void init() throws SQLException
+	{
+		Statement stmt;
+		
+		MySql mySql = new MySql();		
+		Connection connection = (Connection) mySql.connect();
+		
+		if (connection == null) {
+			System.out.println("Connection Error!!");
+		}else{
+			stmt = (Statement) connection.createStatement();
+	        String sql = "SELECT DISTINCT primary_type FROM dataset";
+	        ResultSet rs = stmt.executeQuery(sql);
+	        int count = 1;
+	        while(rs.next()){
+	        	String type = rs.getString("primary_type");
+	        	enCoding.put(type, ""+count);
+	        	deCoding.put(""+count, type);
+	        	count++;
+	        }
+		}
+	}
+	
+	public HashMap<String, String> getEncoding(){
+		return enCoding;
+	}
+	
+	public HashMap<String, String> getDecoding(){
+		return deCoding;
+	}
+}
