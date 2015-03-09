577a6c867fd0c281f93b7fa09e76a0e50b498d8a
Sanket Mehta <sanketvmehta@outlook.com>
2015-03-09 11:27:17 +0530

    Added method to generate candidates for size3

--- Titan/src/Colocation.java
+++ Titan/src/Colocation.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +9,7 @@ import java.lang.Enum;
 import javax.validation.constraints.Min;
 
 import org.antlr.grammar.v3.ANTLRv3Parser.notSet_return;
+import org.apache.cassandra.cli.CliParser.newColumnFamily_return;
 import org.apache.cassandra.cli.CliParser.typeIdentifier_return;
 import org.apache.lucene.util.fst.PairOutputs;
 
@@ -57,13 +59,14 @@ public class Colocation {
 		}
 	}
 	
-	public static void L2 (){
+	public static HashSet<List<String>> L2(){
 		/*
 		 * Generate colocations of size 2
 		 * Iterate over all edges of the graph
 		 */
 		
 		HashMap<String, HashMap<Long, Boolean>> global_count = new HashMap<String, HashMap<Long, Boolean>>();
+		HashMap<String, HashSet<String>> temp_C3 = new HashMap<String, HashSet<String>>();
 		
 		System.out.println("Generating colocations of size 2.\n");
 		int counter = 0;
@@ -134,12 +137,59 @@ public class Colocation {
 				System.out.println(PI);
 				System.out.println("\n");
 				counter+=1;
+				
+				if(temp_C3.containsKey(type1)==false){
+					HashSet<String> tempset = new HashSet<String>();
+					tempset.add(type2);
+					temp_C3.put(type1, tempset);
+				}
+				else{
+					temp_C3.get(type1).add(type2);
+				}
 			}
 		}
 		
 		if (verbose){
 			System.out.println("Total no. of colocations of size 2  are = "+counter);
-		}	
+		}
+		
+		HashSet<List<String>> C3 = new HashSet<List<String>>();
+		
+		
+//		HashMap<String, HashSet<String>> temp_C3 = new HashMap<String, HashSet<String>>();
+		
+		it1 = temp_C3.entrySet().iterator();
+		Iterator it2, it3;
+		
+		String string1, string2, string3;
+		
+		it1 = temp_C3.entrySet().iterator();
+		while(it1.hasNext()){
+			Map.Entry pair1 = (Map.Entry)it1.next();
+			string1 = (String) pair1.getKey(); 
+			it2 = ((Iterable) pair1.getValue()).iterator();
+			while(it2.hasNext()){
+				string2 = (String) it2.next();
+				it3 = temp_C3.get(string2).iterator();
+				while(it3.hasNext()){
+					string3 = (String) it3.next();
+					if(temp_C3.get(string1).contains(string3))
+					{
+						System.out.println("\n");
+						System.out.println(string1);
+						System.out.println(string2);
+						System.out.println(string3);
+						System.out.println("\n");
+						List<String> temp_list = new ArrayList();
+						temp_list.add(string1);
+						temp_list.add(string2);
+						temp_list.add(string3);
+						C3.add(temp_list);
+					}
+				}
+			}
+		}
+		return C3;	
 	}
 	
 	public static void Lk(){
