bbad25970acc5298fb73d1f32884e4c2f6f8effa
Shagun <sshagunsodhani@gmail.com>
2015-03-20 22:04:03 +0530

    Added types.php

---
+++ visualisation/types.php
@@ -0,0 +1,13 @@
+<ul class="nav nav-sidebar">
+<?php
+    require_once('db.php');
+    $conn = connect();
+    $sql = "SELECT DISTINCT primary_type FROM `dataset`";
+    $result = query($conn, $sql);
+    $conn = null;
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) 
+    {
+        echo "<li><a href=\"index.php?type=".$row['primary_type']."\">".$row['primary_type']."</a></li>\n";
+    }
+?>
+</ul>
