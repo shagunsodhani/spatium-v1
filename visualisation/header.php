fcc3e98425f476cb8242d1511be347b29117559a
Shagun <sshagunsodhani@gmail.com>
2015-03-20 15:40:24 +0530

    seperated header

---
+++ visualisation/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport/" content="width=device-width, initial-scale=1">
+        <title>Spatium</title>
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="bootstrap/css/spatium.css" rel="stylesheet">
+        <style type="text/css">
+            html, body, .container-fluid, .row, .main, #map-canvas { height: 100%; margin-left: 0; padding-left: 5%;}
+        </style>
+    </head>
