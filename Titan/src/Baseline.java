1ca1f6900cfb7f4e1e8f427f21fb27b4f36c6a0b
Shagun Sodhani <sshagunsodhani@gmail.com>
2015-03-31 12:19:47 +0530

    Added method to find size 2 frequent colocations using graph traversal alone

--- Titan/src/Baseline.java
+++ Titan/src/Baseline.java
@@ -22,6 +22,8 @@ import com.tinkerpop.blueprints.Compare;
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.gremlin.java.GremlinPipeline;
+import com.tinkerpop.pipes.PipeFunction;
 
 public class Baseline {
 	
@@ -106,6 +108,163 @@ public class Baseline {
 		return Ckplus1;
 	}
 	
+	public static void print_Frequent(HashMap<String, HashMap<String, Float>> Lk, int k){
+		
+		System.out.println("Frequent Colocations of Size "+k+" with their participation index");
+		int counter = 0;
+		Iterator it,it1;
+		it = Lk.entrySet().iterator();
+		while(it.hasNext()){
+			
+			Map.Entry pair = (Map.Entry)it.next();
+			String type1 = (String) pair.getKey();
+			it1 = ((HashMap<String, Double>) pair.getValue()).entrySet().iterator();
+			
+			while(it1.hasNext()){
+				
+				Map.Entry pair1 = (Map.Entry)it1.next();
+				String type2 = (String) pair1.getKey();
+				float pi = (Float) pair1.getValue();
+				System.out.println(type1+":"+type2+" = "+pi);
+//				System.out.println(type1+":"+type2);
+				counter++;
+			}
+		}
+		System.out.println();
+		System.out.println("Total frequent colocations of size "+k+" are "+counter);
+	}
+	
+	public static HashMap<String, HashMap<String, Float>> L2(HashSet<List<String>> Ck){
+		/*
+		 * Generate colocations of size 2
+		 * Iterate over all edges of the graph
+		 */
+		
+		HashMap<String, HashMap<Long, Boolean>> global_count = new HashMap<String, HashMap<Long, Boolean>>();
+		HashMap<String, HashMap<String, Float>> L2 = new HashMap<String, HashMap<String,Float>>();
+		
+		long time1 = System.currentTimeMillis();
+		
+		if(verbose){
+			System.out.println("Generating colocations of size 2.\n");
+		
+		}
+		int counter = 0;
+		//could lead to buffer-overflow
+		
+//		GremlinPipeline pipe = new GremlinPipeline();
+		
+		Iterator it;
+		it = Ck.iterator();
+		String candidate, type1, type2;
+		while(it.hasNext()){
+			List<String> tempList = ((List<String>)it.next());
+			type1 = tempList.get(0);
+			type2 = tempList.get(1);
+			candidate = type1+"-"+type2;
+			
+//			System.out.println("Candidate being screwed right now is : "+candidate);
+			
+			final List<Vertex> temp = new ArrayList<Vertex>();
+			GremlinPipeline pipe = new GremlinPipeline();
+			pipe.start(graph.getVertices("type",type1)).sideEffect(new PipeFunction<Vertex, Vertex>(){
+				public Vertex compute(Vertex argument){
+					if(temp.size()>0){
+						temp.remove(temp.size()-1);
+					}
+					temp.add(argument);
+					return argument;
+				}
+				}).in(candidate).out(candidate).filter(new PipeFunction<Vertex,Boolean>() {
+				  public Boolean compute(Vertex argument) {
+					  if(temp.contains(argument)){
+						  return true;
+					  }
+					  else{
+						  return false;
+					  }				  
+					  }
+					}).path(new PipeFunction<Vertex, Long>(){
+				public Long compute(Vertex argument) {
+					return (Long) argument.getId();
+				}
+			}).enablePath();
+			
+			if(global_count.containsKey(type1+"-"+type2)==false){
+				HashMap<Long, Boolean> default_hashmap = new HashMap<Long, Boolean>();
+				global_count.put(type1+"-"+type2, default_hashmap);
+			}
+			if(global_count.containsKey(type2+"-"+type1)==false){
+				HashMap<Long, Boolean> default_hashmap = new HashMap<Long, Boolean>();
+				global_count.put(type2+"-"+type1, default_hashmap);
+			}
+			
+				
+			Iterator pit = pipe.iterator();
+			
+			while(pit.hasNext()){
+				
+//				System.out.println(pit.next().get(0).getClass());
+				List<Long> tempList1 = ((List<Long>)pit.next());
+				Long Id1 = tempList1.get(0);
+				Long Id2 = tempList1.get(1);
+//				//can be made cleaner if need be...but its java in the end so how much cleaner could it be :P
+				if(global_count.get(type1+"-"+type2).containsKey(Id1) == false ){
+					global_count.get(type1+"-"+type2).put(Id1, true);
+				}
+				if(global_count.get(type2+"-"+type1).containsKey(Id2) == false ){
+					global_count.get(type2+"-"+type1).put(Id2, true);
+				}
+			}
+			
+			Double x1 = (double) global_count.get(type1+"-"+type2).size();
+			Double x2 = (double) global_count.get(type2+"-"+type1).size();
+			Double a = x1/total_count.get(type1);
+			Double b = x2/total_count.get(type2);
+			float PI = (float) java.lang.Math.min(a, b);
+
+			if (verbose)
+			{
+				System.out.println(type1);
+				System.out.println(type2);
+				System.out.println(x1+" / "+total_count.get(type1)+" "+type1);
+				System.out.println(x2+" / "+total_count.get(type2)+" "+type2);
+				System.out.println(PI);
+				System.out.println("\n\n");
+			}
+			if(PI >= PI_threshold)
+			{
+				if(verbose){
+					System.out.println(type1+":"+type2);
+					System.out.println(PI);
+					System.out.println("\n");
+				}
+				counter+=1;
+				
+				if(L2.containsKey(type1)==false){
+					HashMap<String, Float> tempHashMap = new HashMap<String, Float>();
+					tempHashMap.put(type2, PI);
+					L2.put(type1, tempHashMap);
+				}
+				else{
+					L2.get(type1).put(type2, PI);
+				}					
+			}
+			
+			
+		}		
+		
+		
+		
+		if (verbose){
+			System.out.println("Total no. of colocations of size 2  are = "+counter);
+		}
+		long time2 = System.currentTimeMillis();
+		System.out.println("Time taken for size 2 : "+(time2-time1));
+		return L2;
+		
+	}
+	
 	public static void print_Candidate(HashSet<List<String>> Ck, int k){
 		int counter = 0;
 		System.out.println("Candidate Colocations of Size "+k);
@@ -130,9 +289,12 @@ public class Baseline {
 		Baseline baseline = new Baseline();
 		
 //		HashMap<String, HashMap<String, Float>> Lk;
-		HashSet<List<String>> Ck = new HashSet<List<String>>();
-		Ck = L1();
-		print_Candidate(Ck, 2);
+		HashSet<List<String>> C2 = new HashSet<List<String>>();
+		HashMap<String, HashMap<String, Float>> C3 = new HashMap<String, HashMap<String, Float>>();
+		C2 = L1();
+//		print_Candidate(Ck, 2);
+		C3 = L2(C2); 
+		print_Frequent(C3, 2);
 		
 		db.close(graph);
 		
