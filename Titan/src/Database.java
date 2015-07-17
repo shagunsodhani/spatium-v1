ef3dac720e4e3c58789ff907781eeacc6d4d4659
Shagun <sshagunsodhani@gmail.com>
2015-07-17 17:09:37 +0530

    Implemented database class as a singleton class

--- Titan/src/Database.java
+++ Titan/src/Database.java
@@ -1,37 +1,42 @@
-import org.apache.cassandra.cli.CliParser.newColumnFamily_return;
-import org.apache.cassandra.thrift.Cassandra.AsyncProcessor.system_add_column_family;
 import org.apache.commons.configuration.BaseConfiguration;
 import org.apache.commons.configuration.Configuration;
-
 import com.thinkaurelius.titan.core.TitanFactory;
 import com.thinkaurelius.titan.core.TitanGraph;
 import com.thinkaurelius.titan.hadoop.HadoopFactory;
 import com.thinkaurelius.titan.hadoop.HadoopGraph;
-import com.thinkaurelius.titan.hadoop.config.TitanHadoopConfiguration;
-import com.thinkaurelius.titan.hadoop.formats.cassandra.TitanCassandraHadoopGraph;
-import com.thinkaurelius.titan.hadoop.formats.util.TitanHadoopGraph;
-import com.thinkaurelius.titan.tinkerpop.rexster.TitanGraphConfiguration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * Class to handle misc database functions
+ *
+ */
 public class Database {
 	
-	public Database()
-	{
-		System.out.println("Default Constructor for Database class called.");
+	private static Database mInstance;
+	
+//	Can make it synchronized later
+	public static Database getInstance(){
+		if(mInstance == null)
+			mInstance = new Database();
+		return mInstance;
 	}
 	
-	public HadoopGraph connectHadoopGraph() throws Exception{
+	private Database()
+	{
+	}
+	
+	public HadoopGraph getHadoopGraph() throws Exception{
 		
 		/**
 		 * Method to connect to the titan-hadoop graph database
 		 */
 		HadoopGraph graph = HadoopFactory.open("config/titan-graphson.properties");
 //		HadoopGraph graph = HadoopFactory.open("config/config.properties");
-		System.out.println("Worked");
+		System.out.println("Connected to Hadoop Graph");
 		return graph;
 	}
 	
@@ -40,29 +45,21 @@ public class Database {
 		System.out.println("Hadoop Graph Closed");
 	}
 	
-	public TitanGraph connect(){
+	public TitanGraph getTitanGraph(){
 		
 		/**
 		 * Method to connect to the graph database
 		 */
 		Properties prop = new Properties();
 		InputStream input = null;
-	 
 		try {	 
 			input = new FileInputStream("/home/precise/spatium/Titan/config/config.properties");
 			prop.load(input);
-	 
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+			input.close();
 		}
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
 		
 		Configuration config = new BaseConfiguration();
 		/*
@@ -75,7 +72,6 @@ public class Database {
 		config.addProperty("storage.batch-loading",prop.getProperty("storage.batch-loading"));
 		config.addProperty("ids.block-size",prop.getProperty("ids.block-size"));
 		config.addProperty("storage.read-time",prop.getProperty("storage.read-time"));
-//		System.out.proprintln(config.getProperties("storage.buffer-size"));
 		
 		/*
 		 * Adding properties related to external indexing
@@ -88,12 +84,10 @@ public class Database {
 		config.addProperty("index.geohash_precision", prop.getProperty("storage.index.geohash_precision"));
 		config.addProperty("index.geohash_prefix", prop.getProperty("storage.index.geohash_prefix"));
 		
-		TitanGraph g = TitanFactory.open(config);
-//		System.out.println("Instantiated Titan Graph Instance");
-		return g;
+		return TitanFactory.open(config);
 	}
 	
-	public void close(TitanGraph graph){
+	public void closeTitanGraph(TitanGraph graph){
 		/**
 		 * Method to close graph database connection
 		 */
